f2a7cfa773883b083aa20afe1014707a7c409984
mortekianond <mortekianond@gmail.com>
2024-09-03 17:24:17 +0700

    atur db baru

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 $host_db    = "localhost";
 $user_db    = "root";
 $pass_db    = "";
-$nama_db    = "iotmonitoring";
+$nama_db    = "hydroiot";
 $koneksi    = mysqli_connect($host_db, $user_db, $pass_db, $nama_db);
